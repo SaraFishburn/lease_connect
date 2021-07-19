712bdaaea1d1746038373d3f6788ba2624f2e4a9
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-07-19 12:44:41 +1000

    Heading test

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>Lease Connect</h1>
       </header>
     </div>
   );
