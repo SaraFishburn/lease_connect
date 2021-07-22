7127085326850d30c813e7f4f17b97c22e980023
Gemma <gemma@gemmaelsom.com>
2021-07-22 15:03:24 +1000

    Built form for maintenance request

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react"
 
-function Maintenance(props) {
+class Maintenance extends Component {
 
-  // logic here
-  
-  return (
-    <>
-    <h1>Hello</h1>;
-    </>
-  )
-}
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        requestSummary:'',
+        description:''
+      };
+    };
+    
+    handleRequestSummary = (event) => {
+        this.setState({
+            requestSummary: event.target.value
+        })
+
+    }
+
+render() {
+    return (
+        <form>
+            <div>
+                <label>Request Summary</label>
+                <input 
+                type ='text' 
+                value = {this.state.requestSummary} 
+                onChange={this.handleRequestSummary} />
+            </div>
+        </form>
+      )
+    }
+    
+   }
 
 export default Maintenance;
