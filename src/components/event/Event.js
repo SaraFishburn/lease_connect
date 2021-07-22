c71be4c208b10bb55000186378515a0d1f3eeba2
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-22 18:21:10 +1000

    only current month events now show

--- src/components/event/Event.js
+++ src/components/event/Event.js
@@ -2,20 +2,21 @@ import Dayjs from "dayjs";
 import "./styles.scss";
 
 const Event = (props) => {
+
   return (
     <div class="event-tile">
-      <div class={`${props.event_type} event-date`}>
+      <div class={`${props.event.event_type} event-date`}>
         {/* use dayjs library to display the desired parts of the datetime */}
-        <div>{Dayjs(props.datetime).format("DD")}</div>
-        <div>{Dayjs(props.datetime).format("MMM").toUpperCase()}</div>
+        <div>{Dayjs(props.event.datetime).format("DD")}</div>
+        <div>{Dayjs(props.event.datetime).format("MMM").toUpperCase()}</div>
       </div>
       <div class="event-info">
         {/* use string interpolation to capitalize the event type */}
         <div>
-          {`${props.event_type}`.charAt(0).toUpperCase() +
-            `${props.event_type}`.slice(1)}
+          {`${props.event.event_type}`.charAt(0).toUpperCase() +
+            `${props.event.event_type}`.slice(1)}
         </div>
-        <div>{Dayjs(props.datetime).format("h:mma")}</div>
+        <div>{Dayjs(props.event.datetime).format("h:mma")}</div>
       </div>
     </div>
   );
