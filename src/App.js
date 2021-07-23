927b80918255476aa768246888a877c4ecaf66dc
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-23 13:10:56 +1000

    hamburger animation working and nav links showing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CalendarPage from "./pages/calendar_page/CalendarPage"
 import NewUser from './components/user/NewUser';
+import Navbar from './components/navbar/Navbar';
 import './global.css'
 import NewHouse from './components/house/NewHouse';
 
@@ -9,12 +10,14 @@ function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
+        <Navbar />
         <h1>Lease Connect</h1>
         <NewUser/>
         <NewHouse />
       </header>
 
-      <CalendarPage></CalendarPage>
+      <CalendarPage />
+
     </div>
   );
 }
