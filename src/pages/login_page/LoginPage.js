b1c3220abdbb747f6937d2498e4aaa7e2cceaa46
Sara Fishburn <fishburn.sara@gmail.com>
2021-07-31 12:02:11 +1000

    fixing for deploy

--- src/pages/login_page/LoginPage.js
+++ src/pages/login_page/LoginPage.js
@@ -5,7 +5,7 @@ function LoginPage(props) {
   return (
     <div className='login-page'>
       <img src="https://res.cloudinary.com/sazza/image/upload/v1627021393/lease_connect_large_yrwpks.svg" alt="lease connect logo" />
-      <LoginForm setUser={props.setUser} setHouse={props.setHouse} />
+      <LoginForm setUser={props.setUser} setHouse={props.setHouse} setUserData={props.setUserData} setRole={props.setRole}/>
     </div>
   )
 }
